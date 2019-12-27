8c39ee51e3180b4d6ff832d9c4194e33a228542e
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-12-27 20:14:08 +0000

    refactor to private method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,23 @@ class DockingStation
   end 
 
   def release_bike
-    raise 'No bike available' if @bikes.empty?
+    raise 'No bike available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'docking station is full' if @bikes.count >= 20
+    raise 'docking station is full' if full?
     @bikes << bike
   end 
+
+  private
+
+  def full? 
+    @bikes.count >= 20
+  end 
+
+  def empty? 
+    @bikes.empty? 
+  end 
+  
 end 
